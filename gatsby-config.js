de6d21fcaea9274075910143c7023adfeee1ef06
David Milosevic <milosevicdavid@gmail.com>
2020-08-02 21:36:51 +0200

    graphql playground

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         } 
     },
     plugins: [
+        `gatsby-plugin-playground`,
         `gatsby-plugin-sass`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-styled-components`,
