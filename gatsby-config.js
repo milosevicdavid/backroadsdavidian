42c1616adb626743f5236c127cb060c0fec0c8a8
David <milosevicdavid@gmail.com>
2020-09-05 10:25:03 +0200

    Env variable literal path added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,26 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.development`,
 })
-
-
 module.exports = {
-    siteMetadata: {
-        title: `Backroads`,
-        description: `Explore awesome worldwide tours and discover what makes each of them unique.Forget your daily routine, say yes to the adventure...`,
-        author: `@milosevicdavid`,
-        data:  {
-            name: "john",
-            age: 24
-        } 
+  siteMetadata: {
+    title: "BackRoads",
+    description:
+      "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
+    author: "@johndoe",
+    data: {
+      name: "john",
+      age: 24,
     },
-    plugins: [
-        `gatsby-plugin-playground`,
-        `gatsby-plugin-sass`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-styled-components`,
-        `gatsby-plugin-transition-link`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`
-            }
-        },
-        {
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
@@ -34,22 +28,11 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`
-                // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-            }
-        }
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
-    ]
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-transition-link`,
+    `gatsby-plugin-playground`,
+  ],
 }
